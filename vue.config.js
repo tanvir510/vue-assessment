785b9c1ae3e8233eaead1c5215427f148a78f9c2
zarif-al <zarif_al96@outlook.com>
2022-08-22 19:25:03 +0600

    Enable css for web components

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
+function enableShadowCss(config) {
+  const configs = [
+    config.module.rule('vue').use('vue-loader'),
+    config.module.rule('css').oneOf('vue-modules').use('vue-style-loader'),
+    config.module.rule('css').oneOf('vue').use('vue-style-loader'),
+    config.module.rule('css').oneOf('normal-modules').use('vue-style-loader'),
+    config.module.rule('css').oneOf('normal').use('vue-style-loader'),
+  ];
+  configs.forEach((c) =>
+    c.tap((options) => {
+      options.shadowMode = true;
+      return options;
+    })
+  );
+}
+
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'development') {
+      enableShadowCss(config);
+    }
+  },
 });
