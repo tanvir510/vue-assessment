29d60ca5df370ae3b1172e1415a9ffc4c91e94ba
zarif-al <zarif_al96@outlook.com>
2022-08-22 19:25:55 +0600

    Overwrite default app with web component example

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <button class="hello-button">Hello World</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import store from '@/store';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
+  store,
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.hello-button {
+  position: absolute;
+  right: 10px;
+  bottom: 10px;
 }
 </style>
