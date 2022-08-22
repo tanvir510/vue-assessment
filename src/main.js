0b056ca301f1c3ffa73e31b98039f7e154ba8c64
zarif-al <zarif_al96@outlook.com>
2022-08-22 19:26:15 +0600

    Create web component

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
+import wrap from '@vue/web-component-wrapper';
 import Vue from 'vue';
 import App from './App.vue';
-import store from './store';
 
-Vue.config.productionTip = false;
+const CustomElement = wrap(Vue, App);
 
-new Vue({
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+window.customElements.define('test-webcomponent', CustomElement);
